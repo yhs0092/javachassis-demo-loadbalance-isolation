05b0b85179c7c639a6576284638f05f290fc29d2
yaohaishi <yaohaishi@huawei.com>
2018-05-29 16:21:29 +0800

    access log component extension test

--- javachassis-demo-loadbalance-isolation-server/src/main/java/com/github/yhs0092/hello/accesslogitem/TestAccessLogItemCreator.java
+++ javachassis-demo-loadbalance-isolation-server/src/main/java/com/github/yhs0092/hello/accesslogitem/TestAccessLogItemCreator.java
@@ -1,23 +1,37 @@
 package com.github.yhs0092.hello.accesslogitem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.servicecomb.transport.rest.vertx.accesslog.element.AccessLogItem;
-import org.apache.servicecomb.transport.rest.vertx.accesslog.element.creator.VertxExtendedAccessLogItemCreator;
-import org.apache.servicecomb.transport.rest.vertx.accesslog.parser.AccessLogItemLocation;
+import org.apache.servicecomb.transport.rest.vertx.accesslog.parser.AccessLogItemMeta;
+import org.apache.servicecomb.transport.rest.vertx.accesslog.parser.VertxRestAccessLogItemCreator;
 
 import io.vertx.ext.web.RoutingContext;
 
-public class TestAccessLogItemCreator
-    implements VertxExtendedAccessLogItemCreator {
-  @Override
-  public AccessLogItem<RoutingContext> create(String rawPattern, AccessLogItemLocation location) {
-    if (null != location.getPlaceHolder()) {
-      return null;
-    }
+public class TestAccessLogItemCreator implements VertxRestAccessLogItemCreator {
+  private List<AccessLogItemMeta> supportedMetaList;
 
-    return new TestAccessLogItem(
-        rawPattern.substring(
-            location.getStart() + TestAccessLogMatcher.PREFIX.length(),
-            location.getEnd() - TestAccessLogMatcher.SUFFIX.length()
-        ));
+  public TestAccessLogItemCreator() {
+    supportedMetaList = new ArrayList<>();
+    supportedMetaList.add(new AccessLogItemMeta("%{", "}XtestItem"));
+    // override ResponseHeaderItem
+    supportedMetaList.add(new AccessLogItemMeta("%{", "}o", -1));
+  }
+
+  @Override
+  public List<AccessLogItemMeta> getAccessLogItemMeta() {
+    return supportedMetaList;
+  }
+
+  @Override
+  public AccessLogItem<RoutingContext> createItem(AccessLogItemMeta accessLogItemMeta, String config) {
+    if ("}XtestItem".equals(accessLogItemMeta.getSuffix())) {
+      return accessLogParam -> config + "[" + accessLogParam.getStartMillisecond() + "]";
+    }
+    if ("}o".equals(accessLogItemMeta.getSuffix())) {
+      return accessLogParam -> "[" + "modified item, config = " + config + "]";
+    }
+    return null;
   }
 }
